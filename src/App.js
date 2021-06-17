39583a86bfe9da2796c47df2c53afdbdd0f2a2a9
DarpanModak39 <71865006+DarpanModak39@users.noreply.github.com>
2021-06-17 16:23:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Tips from "./Tips";
+
+function App() {
+  return (
+    <>
+      <h1>Photography Tips</h1>
+      <Tips />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Tips.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Tips = () => {
+  return (
+    <>
+      <p>
+        <h2>Expose and focus first, then frame your shot</h2>
+        An improperly exposed or blurry picture is unusable, but one not
+        precisely framed may still be saved. For this reason, you should always
+        focus on and properly expose for the subject before adjusting the frame.
+        This is something that happens more often when you have extreme lights
+        and darks in the same scene.
+      </p>
+
+      <p>
+        <h2>Focus on the eyes</h2>
+        We are always drawn towards the eyes in a photograph, since eyes are a
+        natural focal point that we connect with. When taking portrait
+        photographs at any aperture, make sure you nail the focus on the eyes.
+        As long as the eyes are in focus, both you and your subject are more
+        likely to consider the picture to be properly shot.
+      </p>
+
+      <p>
+        <h2>Straighten and crop when editing</h2>
+        You should try to straighten shots by looking through your camera’s
+        viewfinder before capturing an image, but it’s not always easy to get
+        this perfect on the first try. The viewfinder or the preview on your LCD
+        is quite small compared to full-screen editing so you may realize it
+        needs adjusting once you see it on a bigger screen. Simply rotate your
+        images in post production software and crop out the empty spaces.
+      </p>
+
+      <p>
+        <h2>Make use of reflections</h2>
+        There are lots of unique opportunities if you pay attention where most
+        people don’t. One of the things to look out for are reflections. You can
+        find them after (or even during) rainy days, in puddles, in lakes and
+        even in swimming pools. Water isn’t the only source, try mirrors, big
+        glass windows, and chromed out fixtures.
+      </p>
+    </>
+  );
+};
+
+export default Tips;
